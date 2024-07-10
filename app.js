6149caa9197bf2f2ab84042182d33adb8438e5f1
cristianccgg <cristianccggg@gmail.com>
2024-07-10 18:16:59 -0500

    finished

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const hamBtn = document.getElementById("ham-btn");
 const mobileMenu = document.getElementById("mobile-menu");
+const productBtns = document.querySelectorAll(".product-btn");
+const productLists = document.querySelectorAll(".product-list");
+const companyBtns = document.querySelectorAll(".company-btn");
+const companyList = document.querySelectorAll(".company-list");
+const connectBtns = document.querySelectorAll(".connect-btn");
+const connectList = document.querySelectorAll(".connect-list");
 
 hamBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
+  if (mobileMenu) {
+    mobileMenu.classList.toggle("hidden");
+  }
+});
+
+productBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    productLists[index].classList.toggle("hidden");
+  });
+});
+
+companyBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    companyList[index].classList.toggle("hidden");
+  });
+});
+
+connectBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    connectList[index].classList.toggle("hidden");
+  });
 });
